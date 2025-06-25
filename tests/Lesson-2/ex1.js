73d71aa9b75af7098316eb4368513f484dfdca52
Phan Thu <phanthu2089@gmail.com>
2025-06-25 23:56:33 +0700

    feat : lesson-2

---
+++ tests/Lesson-2/ex1.js
@@ -0,0 +1,5 @@
+const Number = 12;
+let name  = "my number";
+ let isEven = false;
+if (Number%2===0) {isEven=true};
+console.log(isEven);
---
+++ tests/Lesson-2/lesson-2.js
@@ -0,0 +1,8 @@
+// nếu là số lẻ : chia dư cho 2 =1
+// nếu là số chẵn chia dư cho 2 = 0
+for (let x = 1; x <= 10; x++) {
+    if (x % 2 === 1) { console.log(x + " là số lẻ") };
+    if (x % 2 === 0) { console.log(x + "là số chẵn") };
+}
+
+
